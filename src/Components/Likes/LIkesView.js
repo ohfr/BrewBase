d1d1d3da8ed8066d166ab33c2844b700cf0b816e
Daniel Martin <danielmartin1205@gmail.com>
2019-09-06 08:31:09 -0400

    delted unused state

--- src/Components/Likes/LIkesView.js
+++ src/Components/Likes/LIkesView.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faBeer} from '@fortawesome/free-solid-svg-icons';
